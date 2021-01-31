5259a0e621cb2778df34c4d171748ce8063d2fab
Aryaman <ladhaaryaman@ucla.edu>
2021-01-30 22:17:28 -0800

    Basic Functionality and Style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,78 @@
 import './App.css';
 import {Component} from "react";
 import Chart from "react-google-charts";
-
+import DropdownButton from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Spinner from 'react-bootstrap/Spinner'
 function numberWithCommas(x) {
+  if(x === null){
+    return '-'
+  }
   x = x.toString();
   var pattern = /(-?\d+)(\d{3})/;
   while (pattern.test(x))
       x = x.replace(pattern, "$1,$2");
   return x;
 }
+const dictionary = {
+  'az':'Arizona',
+  'al':'Alabama',
+  'ak':'Alaska',
+  'as':'American Samoa',
+  'ar':'Arkansas',
+  'ca':'California',
+  'co':'Colorado',
+  'ct':'Connecticut',
+  'de':'Delaware',
+  'dc':'District of Columbia',
+  'fl':'Florida',
+  'ga':'Georgia',
+  'gu':'Guam',
+  'hi':'Hawaii',
+  'id':'Idaho',
+  'il':'Illinois',
+  'in':'Indiana',
+  'ia':'Iowa',
+  'ks': 'Kansas',
+  'ky': 'Kentucky',
+  'la': 'Louisiana',
+  'me': 'Maine',
+  'md': 'Maryland',
+  'ma': 'Massachusetts',
+  'mi': 'Michigan',
+  'mn': 'Minnesota',
+  'ms': 'Mississippi',
+  'mo': 'Missouri',
+  'mt': 'Montana',
+  'ne':'Nebraska',
+  'nv':'Nevada',
+  'nh':'New Hampshire',
+  'nj':'New Jersey',
+  'nm': 'New Mexico',
+  'ny': 'New York',
+  'nc': 'North Carolina',
+  'nd': 'North Dakota',
+  'mp': 'Northern Mariana Islands',
+  'oh': 'Ohio',
+  'ok': 'Oklahoma',
+  'or': 'Oregon',
+  'pa': 'Pennsylvania',
+  'pr': 'Puerto Rico',
+  'ri': 'Rhode Island',
+  'sc': 'South Carolina',
+  'sd': 'South Dakota',
+  'tn': 'Tennessee',
+  'tx': 'Texas',
+  'ut': 'Utah',
+  'vt': 'Vermont',
+  'va': 'Virginia',
+  'vi': 'Virgin Islands',
+  'wa': 'Washington',
+  'wv': 'West Virginia',
+  'wi': 'Wisconsin',
+  'wy': 'Wyoming',
+  'us' : 'United States'
+};
 
 class App extends Component {
   constructor(){
@@ -21,7 +85,61 @@ class App extends Component {
       states_wanted: false,
       state_name : "ca",
       state_array:{
-        ca: { fetched: false, data: []}
+        "al": { fetched: false, data: []},
+        "ak": { fetched: false, data: []},
+        "as": { fetched: false, data: []},
+        "az": { fetched: false, data: []},
+        "ar": { fetched: false, data: []},
+        "ca": { fetched: false, data: []},
+        "co": { fetched: false, data: []},
+        "ct": { fetched: false, data: []},
+        "de": { fetched: false, data: []},
+        "dc": { fetched: false, data: []},
+        "fl": { fetched: false, data: []},
+        "ga": { fetched: false, data: []},
+        "gu": { fetched: false, data: []},
+        "hi": { fetched: false, data: []},
+        "id": { fetched: false, data: []},
+        "il": { fetched: false, data: []},
+        "in": { fetched: false, data: []},
+        "ia": { fetched: false, data: []},
+        "ks": { fetched: false, data: []},
+        "ky": { fetched: false, data: []},
+        "la": { fetched: false, data: []},
+        "me": { fetched: false, data: []},
+        "md": { fetched: false, data: []},
+        "ma": { fetched: false, data: []},
+        "mi": { fetched: false, data: []},
+        "mn": { fetched: false, data: []},
+        "ms": { fetched: false, data: []},
+        "mo": { fetched: false, data: []},
+        "ne": { fetched: false, data: []},
+        "nv": { fetched: false, data: []},
+        "nh": { fetched: false, data: []},
+        "nj": { fetched: false, data: []},
+        "nm": { fetched: false, data: []},
+        "ny": { fetched: false, data: []},
+        "nc": { fetched: false, data: []},
+        "nd": { fetched: false, data: []},
+        "mp": { fetched: false, data: []},
+        "oh": { fetched: false, data: []},
+        "ok": { fetched: false, data: []},
+        "or": { fetched: false, data: []},
+        "pa": { fetched: false, data: []},
+        "pr": { fetched: false, data: []},
+        "ri": { fetched: false, data: []},
+        "sc": { fetched: false, data: []},
+        "sd": { fetched: false, data: []},
+        "tn": { fetched: false, data: []},
+        "tx": { fetched: false, data: []},
+        "ut": { fetched: false, data: []},
+        "vt": { fetched: false, data: []},
+        "va": { fetched: false, data: []},
+        "vi": { fetched: false, data: []},
+        "wa": { fetched: false, data: []},
+        "wv": { fetched: false, data: []},
+        "wi": { fetched: false, data: []},
+        "wy": { fetched: false, data: []},
       }
     };
   }
@@ -30,7 +148,7 @@ class App extends Component {
     var data_array = [];
     var now = new Date();
     var daysOfYear = [];
-    for (var d = new Date(2020, 11, 31); d <= now; d.setDate(d.getDate() + 1)) { //In build version, set date to 2020, 0, 14
+    for (var d = new Date(2020, 11, 31); d <= now; d.setDate(d.getDate() + 1)) { //In build version, set date to 2020, 3, 3
       //On the test version, use 2020, 11, 31
       var date = new Date(d);
       //console.log(date);
@@ -57,17 +175,17 @@ class App extends Component {
     for(var i = 0; i< daysOfYear.length; i++){
         try{
           var res = await fetch(daysOfYear[i]);
-          var data = await res.json();
-          console.log(data);
+          var data = res.json();
+          //console.log(data);
           data_array.push(data);
         }
         catch{
-          console.log("F");
+          //console.log("F");
         }
     }
     var state_array = this.state.state_array;
-    state_array.ca.data = data_array;
-    state_array.ca.fetched = true;
+    state_array[state_name].data = data_array;
+    state_array[state_name].fetched = true;
     this.setState({
       state_array: state_array
     });
@@ -83,7 +201,7 @@ class App extends Component {
         states_wanted: true,
         state_name:  name
       });
-      if(!this.state.state_array.ca.fetched){
+      if(!this.state.state_array[name].fetched){
         await this.fetchStateData(name);
       }
     }
@@ -94,7 +212,7 @@ class App extends Component {
     var data_array = [];
     var now = new Date();
     var daysOfYear = [];
-    for (var d = new Date(2020, 11, 31); d <= now; d.setDate(d.getDate() + 1)) { //In build version, set date to 2020, 0, 14
+    for (var d = new Date(2020, 11, 31); d <= now; d.setDate(d.getDate() + 1)) { //In build version, set date to 2020, 3, 3
       //On the test version, use 2020, 11, 31
       var date = new Date(d);
       //console.log(date);
@@ -122,11 +240,11 @@ class App extends Component {
         try{
           var res = await fetch(daysOfYear[i]);
           var data = await res.json();
-          console.log(data);
+          //console.log(data);
           data_array.push(data);
         }
         catch{
-          console.log("F");
+          //console.log("F");
         }
     }
     this.setState({
@@ -141,30 +259,49 @@ class App extends Component {
       //   })
       //   .catch(console.log)
       // }
-    console.log(data_array.length);
+    //console.log(data_array.length);
   }
   render(){
     var display_data = []
+    var display_name = 'United States';
     if(!this.state.fetched){
-      return (<div> Loading Data</div>);
+      return (
+      <Spinner animation="border" role="status">
+          <span className="sr-only">Here...</span>
+      </Spinner>
+      // <div> Loading Data</div>
+      );
     }
     if(this.state.states_wanted){
-      if(!this.state.state_array.ca.fetched){
-        return (<div>Loading Data</div>);
+      var name = this.state.state_name;
+      if(!this.state.state_array[name].fetched){
+        return (
+          <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+          </Spinner>
+        // <div>Loading Data</div>
+        );
       }
       else{
-        display_data = this.state.state_array.ca.data;
+        display_data = this.state.state_array[name].data;
       }
+      display_name = dictionary[name];
     }
     else{
       display_data = this.state.data_array;
     }
+    var display_date;
     var graph_total = []
     var graph_daily_new = []
-    graph_total.push(['x', 'Total Cases'])
-    graph_daily_new.push(['x', 'Daily New Cases'])
+    var graph_daily_deaths = []
+    var graph_deaths = []
+    graph_deaths.push(['x','Total Deaths']);
+    graph_daily_deaths.push(['x','New Deaths']);
+    graph_total.push(['x', 'Total Cases']);
+    graph_daily_new.push(['x', 'New Cases']);
     var slide_begin;
     var slide_end;
+    console.log(display_data);
     for(var i = 0; i< display_data.length-1; i++){
       //console.log(this.state.data_array)
       var u = display_data[i].date; //2020101
@@ -174,50 +311,53 @@ class App extends Component {
       var dd = parseInt(d.substring(6,8));// 01
       //var format_date = yy+ "-" + mm + "-" +dd;// "2020-01-01"
       var x = new Date(yy,mm,dd);
-      console.log(x);
+      //console.log(x);
       graph_daily_new.push([x,display_data[i].positiveIncrease]);
       graph_total.push([x,display_data[i].positive]);
+      graph_daily_deaths.push([x,display_data[i].deathIncrease]);
+      graph_deaths.push([x,display_data[i].death]);
+
       if(i === 0){
         slide_begin = x;
       }
-      if(i === this.state.data_array.length-2){
+      if(i === this.state.data_array.length-1){
+        console.log("Here");
+        display_date = x.toString().substring(4,15);
+        console.log(display_date);
         slide_end = x;
       }
     }
-    var total_cases = display_data[display_data.length-2].positive;
-    var new_cases = display_data[display_data.length-2].positiveIncrease;
+    var total_hospitalized = display_data[display_data.length-1].hospitalizedCumulative;
+    var new_hospitalized = display_data[display_data.length-1].hospitalizedIncrease;
+    var deaths = display_data[display_data.length-1].death;
+    var new_deaths = display_data[display_data.length-1].deathIncrease;
+    var total_cases = display_data[display_data.length-1].positive;
+    var new_cases = display_data[display_data.length-1].positiveIncrease;
     return (
-      <div>
-        <div>
-          Total Cases as of Today: {numberWithCommas(total_cases)}
-        </div>
-        <div>
-          New Cases today: {numberWithCommas(new_cases)}
-        </div>
-        <div>
-          <button onClick= {async () => await this.changeFilter("ca")}> California</button>
-          <button onClick= {async () => await this.changeFilter("us")}> Reset</button>
-        </div>
-      <div className="App">
+      <body>
+      <div className="wrapper">
+        <div className="left_column">
+        <div className="graph_heading">{slide_end} </div>
+        <span className="graph_heading">Total Cases </span>
         <Chart
-          width={'600px'}
+          width={'800px'}
           height={'400px'}
           chartType="LineChart"
           loader={<div>Loading Chart</div>}
           data={graph_total}
-          options={{
-            hAxis: {
-              title: 'Time',
-            },
-            vAxis: {
-              title: 'Total cases',
-            }
+          // options={{
+          //   hAxis: {
+          //     title: 'Time',
+          //   },
+          //   vAxis: {
+          //     title: 'Total cases',
+          //   }
             // chartArea: { height: '400px', width: '600px' },
             // //hAxis: { slantedText: false },
             // vAxis: { viewWindow: { min: 0, max: 2000 } },
             // legend: { position: 'none' },
             //
-          }}
+          //}}
           rootProps={{ 'data-testid': '1' }}
           // chartPackages={['corechart', 'controls']}
           // controls={[
@@ -242,26 +382,140 @@ class App extends Component {
           //   },
           // ]}
         />
-      </div>
-      <div>
+        <span className="graph_heading"> Daily New Cases </span>
       <Chart
-          width={'600px'}
+          width={'800px'}
           height={'400px'}
           chartType="LineChart"
           loader={<div>Loading Chart</div>}
           data={graph_daily_new}
-          options={{
-            hAxis: {
-              title: 'Time',
-            },
-            vAxis: {
-              title: 'Daily New Cases',
-            },
-          }}
+          // options={{
+          //   hAxis: {
+          //     title: 'Time',
+          //   },
+          //   vAxis: {
+          //     title: 'Daily New Cases',
+          //   },
+          // }}
+          rootProps={{ 'data-testid': '1' }}
+        />
+        <span className="graph_heading"> Total Deaths </span>
+      <Chart
+          width={'800px'}
+          height={'400px'}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={graph_deaths}
+          // options={{
+          //   hAxis: {
+          //     title: 'Time',
+          //   },
+          //   vAxis: {
+          //     title: 'Total Deaths',
+          //   },
+          // }}
+          rootProps={{ 'data-testid': '1' }}
+        />
+        <span className="graph_heading"> New Deaths </span>
+      <Chart
+          width={'800px'}
+          height={'400px'}
+          chartType="LineChart"
+          loader={<div>Loading Chart</div>}
+          data={graph_daily_deaths}
+          // options={{
+          //   hAxis: {
+          //     title: 'Time',
+          //   },
+          //   vAxis: {
+          //     title: 'New Deaths',
+          //   },
+          // }}
           rootProps={{ 'data-testid': '1' }}
         />
       </div>
+      <div id="right_column">
+        <div className="country heading">
+          <u>{display_name}</u>
+        </div>
+        <div className="statistics">
+        <div className="numbers">{numberWithCommas(total_cases)}</div>
+        <div className="heading">Total cases</div> 
+        <hr align='left'/>
+        <div className="numbers">{numberWithCommas(new_cases)}</div> 
+        <div className="heading">New cases</div>
+        <hr align='left'/>
+        <div className="numbers">{numberWithCommas(deaths)}</div>
+        <div className="heading">Total deaths</div> 
+        <hr align='left'/>
+        <div className="numbers">{numberWithCommas(new_deaths)}</div> 
+        <div className="heading">New deaths</div>  
+        <hr align='left'/>
+        <div className="numbers">{numberWithCommas(total_hospitalized)}</div> 
+        <div className="heading">Total cumulative hosptalizations</div>
+        <hr align='lwft'/>
+        <div className="numbers">{numberWithCommas(new_hospitalized)}</div> 
+        <div className="heading">New hosptalizations</div> 
+        <hr align="left"/>
+        </div>
+        <button onClick= {async () => await this.changeFilter("al")}> Alabama</button>
+        <button onClick= {async () => await this.changeFilter("ak")}> Alaska</button>
+        <button onClick= {async () => await this.changeFilter("as")}> American Samoa</button>
+        <button onClick= {async () => await this.changeFilter("az")}> Arizona</button>
+        <button onClick= {async () => await this.changeFilter("ar")}> Arkansas</button>
+        <button onClick= {async () => await this.changeFilter("ca")}> California</button>
+        <button onClick= {async () => await this.changeFilter("co")}> Colorado</button>
+        <button onClick= {async () => await this.changeFilter("ct")}> Connecticut</button>
+        <button onClick= {async () => await this.changeFilter("de")}> Delaware</button>
+        <button onClick= {async () => await this.changeFilter("dc")}> District of Columbia</button>
+        <button onClick= {async () => await this.changeFilter("fl")}> Florida</button>
+        <button onClick= {async () => await this.changeFilter("ga")}> Georgia</button>
+        <button onClick= {async () => await this.changeFilter("gu")}> Guam</button>
+        <button onClick= {async () => await this.changeFilter("hi")}> Hawaii</button>
+        <button onClick= {async () => await this.changeFilter("id")}> Idaho</button>
+        <button onClick= {async () => await this.changeFilter("il")}> Illinois</button>
+        <button onClick= {async () => await this.changeFilter("in")}> Indiana</button>
+        <button onClick= {async () => await this.changeFilter("ia")}> Iowa</button>
+        <button onClick= {async () => await this.changeFilter("ks")}> Kansas</button>
+        <button onClick= {async () => await this.changeFilter("ky")}> Kentucky</button>
+        <button onClick= {async () => await this.changeFilter("la")}> Lousiana</button>
+        <button onClick= {async () => await this.changeFilter("me")}> Maine</button>
+        <button onClick= {async () => await this.changeFilter("md")}> Marlyand</button>
+        <button onClick= {async () => await this.changeFilter("ma")}> Massachusetts</button>
+        <button onClick= {async () => await this.changeFilter("mi")}> Michigan</button>
+        <button onClick= {async () => await this.changeFilter("mn")}> Minnesota</button>
+        <button onClick= {async () => await this.changeFilter("ms")}> Mississipi</button>
+        <button onClick= {async () => await this.changeFilter("mo")}> Missouri</button>
+        <button onClick= {async () => await this.changeFilter("mt")}> Montana</button>
+        <button onClick= {async () => await this.changeFilter("ne")}> Nebraska</button>
+        <button onClick= {async () => await this.changeFilter("nv")}> Nevada</button>
+        <button onClick= {async () => await this.changeFilter("nm")}> New Mexico</button>
+        <button onClick= {async () => await this.changeFilter("ny")}> New York</button>
+        <button onClick= {async () => await this.changeFilter("nc")}> North Carolina</button>
+        <button onClick= {async () => await this.changeFilter("nd")}> North Dakota</button>
+        <button onClick= {async () => await this.changeFilter("mp")}> Northern Mariana Islands</button>
+        <button onClick= {async () => await this.changeFilter("oh")}> Ohio</button>
+        <button onClick= {async () => await this.changeFilter("ok")}> Oklahoma</button>
+        <button onClick= {async () => await this.changeFilter("or")}> Oregon</button>
+        <button onClick= {async () => await this.changeFilter("pa")}> Pennsylvania</button>
+        <button onClick= {async () => await this.changeFilter("pr")}> Puerto Rico</button>
+        <button onClick= {async () => await this.changeFilter("ri")}> Rhode Island</button>
+        <button onClick= {async () => await this.changeFilter("sc")}> South Carolina</button>
+        <button onClick= {async () => await this.changeFilter("sd")}> South Dakota</button>
+        <button onClick= {async () => await this.changeFilter("tn")}> Tennessee</button>
+        <button onClick= {async () => await this.changeFilter("tx")}> Texas</button>
+        <button onClick= {async () => await this.changeFilter("ut")}> Utah</button>
+        <button onClick= {async () => await this.changeFilter("vt")}> Vermont</button>
+        <button onClick= {async () => await this.changeFilter("vi")}> Virginia</button>
+        <button onClick= {async () => await this.changeFilter("wa")}> Washington</button>
+        <button onClick= {async () => await this.changeFilter("wv")}> West Virgina</button>
+        <button onClick= {async () => await this.changeFilter("wi")}> Wisconsin</button>
+        <button onClick= {async () => await this.changeFilter("wy")}> Wyoming</button>
+        <button onClick= {async () => await this.changeFilter("us")}> Reset</button>
       </div>
+    </div>
+
+    </body>
     );
   }
 }
